22a10f5515bfc29a7ff36c17342ba9fdb90c0e58
Przemek Rudzki <przemek@kritik.io>
2021-11-17 11:20:56 -0500

    handle display/add task from main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react"
+import "./App.css"
+import "./components/TodoList"
+import TodoList from "./components/TodoList"
+import AddTodo from "./components/AddTodo"
+
+type todoItem = { id: string; text: string }
 
 function App() {
+  const [todoList, setTodoList] = useState<todoItem[]>([])
+
+  const handleAddToDo = (text: string) => {
+    setTodoList([...todoList, { id: Math.random().toString(), text }])
+  }
   return (
     <div className="App">
       <h1>Simple note taker</h1>
+      <div>
+        <AddTodo onAddTodo={handleAddToDo} />
+        <TodoList todoList={todoList} />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
