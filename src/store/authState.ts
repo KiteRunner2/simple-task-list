6f103f897a45a0b442a218500c946f687c87a167
Przemek Rudzki <przemek@kritik.io>
2021-12-05 15:46:49 -0500

    add register state

--- src/store/authState.ts
+++ src/store/authState.ts
@@ -1,10 +1,16 @@
 const createAuthStateSlice = (set: any, get: any) => {
   return {
     isAuthenticated: false,
+    isRegistering: false,
     user: {},
     setAuthenticated: (authState: boolean) => {
       set((state: any) => {
         return { isAuthenticated: authState }
+      })
+    },
+    setIsRegistering: (registeringState: boolean) => {
+      set((state: any) => {
+        return { isRegistering: registeringState }
       })
     },
   }
