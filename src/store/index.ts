2e46f5f051608904a3343e5ba43e48f28248695a
Przemek Rudzki <przemek@kritik.io>
2021-12-05 15:46:14 -0500

    add error state

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,11 +3,13 @@ import { devtools } from "zustand/middleware"
 import toDo from "./todo"
 import loadingState from "./loadingState"
 import authState from "./authState"
+import createErrorStateSlice from "./errorState"
 
 const store = (set: any, get: any) => ({
   ...toDo(set, get),
   ...loadingState(set, get),
   ...authState(set, get),
+  ...createErrorStateSlice(set, get),
 })
 
 const useStore = create(devtools(store))
