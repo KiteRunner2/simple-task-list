9e1bf3104c89dc0ea8bc10aef2cbbc433a67f724
Przemek Rudzki <przemek@kritik.io>
2022-02-01 10:32:17 -0500

    Add process.on events for SIGTERM and SIGINT

--- server/app/index.ts
+++ server/app/index.ts
@@ -12,6 +12,18 @@ InitExpress(app)
 
 connectToDatabase()
 
+// TODO Add gracefull shutdown for database connection
+// Add gracefull shutdown for database connection
+process.on("SIGTERM", () => {
+  logger.log({ level: "info", message: "Terminating!" })
+  process.exit(1)
+})
+
+process.on("SIGINT", () => {
+  logger.log({ level: "info", message: "Terminating!" })
+  process.exit(1)
+})
+
 app.listen(PORT, () => {
   logger.log({ level: "info", message: `Application started on port ${PORT}` })
 })
